c345a57ce4ae6b23af7273d3caa52c7d9af8180a
Bryan <bryan.yi.liu@gmail.com>
2023-12-29 13:41:06 +0800

    feat: come CSS

--- frontend/src/components/PortfolioPage.js
+++ frontend/src/components/PortfolioPage.js
@@ -1,13 +1,14 @@
 import React, { useState, useMemo } from 'react';
 import { useTable, useSortBy } from 'react-table';
 import { evaluate } from 'mathjs';
-import { mockCompanies } from '../data/mockData'; // Adjust this import to your file structure
+import { mockCompanies } from '../data/mockData'; 
 
 const PortfolioPage = () => {
   const [data, setData] = useState(mockCompanies);
   const [customFormulas, setCustomFormulas] = useState(['']);
   const [customResults, setCustomResults] = useState({});
   const [customFormulaErrors, setCustomFormulaErrors] = useState([]);
+
 
   const applyCustomFormulas = () => {
     const newCustomResults = {};
@@ -51,6 +52,8 @@ const PortfolioPage = () => {
     setCustomFormulas(newFormulas);
   };
 
+
+
   const keyToHeaderMapping = {
     S1: 'Scope 1',
     S2: 'Scope 2',
@@ -70,6 +73,8 @@ const PortfolioPage = () => {
     Children: "Children's Rgts",
     Corruption: 'Anti Corruption',
   };
+
+  
 
   const columns = useMemo(() => [
     {
@@ -100,28 +105,28 @@ const PortfolioPage = () => {
     {
       Header: 'Quantitative',
       columns: Object.keys(mockCompanies[0].quantitative).map(key => ({
-        Header: keyToHeaderMapping[key] || key, // Use the mapping if it exists, otherwise use the key itself
+        Header: keyToHeaderMapping[key] || key,
         accessor: d => d.quantitative[key],
       })),
     },
     {
       Header: 'Qualitative',
       columns: Object.keys(mockCompanies[0].qualitative).map(key => ({
-        Header: keyToHeaderMapping[key] || key, // Use the mapping if it exists, otherwise use the key itself
+        Header: keyToHeaderMapping[key] || key,
         accessor: d => d.qualitative[key],
       })),
     },
-    // Custom Scores columns
-    ...customFormulas.map((_, index) => ({
-      Header: `Custom Score ${index + 1}`,
-      columns: [
-        {
-          Header: `Score ${index + 1}`,
-          accessor: d => customResults[d.comp_key]?.[`score${index + 1}`] || '',
-        },
-      ],
-    })),
+    // Custom Scores column
+    {
+      Header: 'Custom',
+      columns: customFormulas.map((_, index) => ({
+        Header: `Score ${index + 1}`,
+        accessor: d => customResults[d.comp_key]?.[`score${index + 1}`] || '',
+      })),
+    },
   ], [customFormulas, customResults]);
+
+  
 
   const tableInstance = useTable({ columns, data }, useSortBy);
 
@@ -134,56 +139,81 @@ const PortfolioPage = () => {
   } = tableInstance;
 
   return (
-    <div>
-      {customFormulas.map((formula, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            placeholder={`Enter custom formula for Score ${index + 1}...`}
-            value={formula}
-            onChange={e => updateCustomFormula(index, e.target.value)}
-          />
-          <button onClick={() => removeCustomFormula(index)}>Remove</button>
-          {customFormulaErrors[index] && <div>{customFormulaErrors[index]}</div>}
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ flex: 1 }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+          <div>
+            <div>Portfolio: NPF Equities All</div>
+            <div>Gross Return: </div>
+          </div>
+          <div>
+            <div> &nbsp; </div>
+            <div>Factor Weights -{'>'} </div>
+            <div>Return Delta -{'>'}</div>
+          </div>
+          <div>
+            <div>Return</div>
+          </div>
+          <div>
+            <div>Volatility</div>
+          </div>
+          <div>
+            <div>Currency: USD</div>
+            <div>Year: 2021</div>
+          </div>
         </div>
-      ))}
-      <button onClick={addCustomFormula}>Add Custom Score</button>
-      <button onClick={applyCustomFormulas}>Apply Custom Formulas</button>
-
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => (
-                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render('Header')}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? ' 🔽'
+                          : ' 🔼'
+                        : ''}
+                    </span>
+                  </th>
                 ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div style={{ minWidth: '300px', padding: '10px' }}> {/* Adjust the width and padding as needed */}
+        {customFormulas.map((formula, index) => (
+          <div key={index} style={{ marginBottom: '10px' }}>
+            <input
+              type="text"
+              placeholder={`Enter custom formula for Score ${index + 1}...`}
+              value={formula}
+              onChange={e => updateCustomFormula(index, e.target.value)}
+              style={{ width: '100%', marginBottom: '5px' }}
+            />
+            <button onClick={() => removeCustomFormula(index)}>Remove</button>
+            {customFormulaErrors[index] && <div style={{ color: 'red' }}>{customFormulaErrors[index]}</div>}
+          </div>
+        ))}
+        <button onClick={addCustomFormula} style={{ marginRight: '5px' }}>Add Custom Score</button>
+        <button onClick={applyCustomFormulas}>Apply Custom Formulas</button>
+      </div>
     </div>
   );
-};
+}
 
 export default PortfolioPage;
