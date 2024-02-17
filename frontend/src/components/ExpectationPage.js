158f4de0f1dbb5e262377d98b9e0c06d935cfc69
Bryan <bryan.yi.liu@gmail.com>
2024-02-17 23:29:17 +0800

    feat: expectation page trial

--- frontend/src/components/ExpectationPage.js
+++ frontend/src/components/ExpectationPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import biodiveristyPolicyData from '../data/biodiversity';
 import climatePolicyData from '../data/climate';
 import waterPolicyData from '../data/water';
 
 
-const BiodiversityPage = () => {
+const BiodiversityPage = ({ weights, onWeightChange }) => {
   return (
     <div>
       <table>
@@ -24,7 +24,13 @@ const BiodiversityPage = () => {
               <td>{policy.indicator}</td>
               <td>{policy.description}</td>
               <td>{policy.category}</td>
-              <td>{policy.weight}</td>
+              <td>
+                <input
+                  type="number"
+                  value={weights[index] || ''}
+                  onChange={(e) => onWeightChange(index, e.target.value)}
+                />
+              </td>
               <td>{policy.sectors}</td>
             </tr>
           ))}
@@ -93,6 +99,16 @@ const WaterPage = () => {
 };
 
 const ExpectationPage = () => {
+  const [biodiversityWeights, setBiodiversityWeights] = useState({});
+
+  const handleBiodiversityWeightChange = (index, value) => {
+    setBiodiversityWeights({...biodiversityWeights, [index]: value});
+  }
+
+  const saveData = () => {
+    console.log('Saving data', {biodiversityWeights});
+  };
+
   return (
     <div>
       <nav>
@@ -102,8 +118,9 @@ const ExpectationPage = () => {
           <li><Link to="water">Water</Link></li>
         </ul>
       </nav>
+      <button onClick={saveData}>Save Data</button>
       <Routes>
-        <Route path="biodiversity" element={<BiodiversityPage />} />
+        <Route path="biodiversity" element={<BiodiversityPage weights={biodiversityWeights} onWeightChange={handleBiodiversityWeightChange} />} />
         <Route path="climate" element={<ClimatePage />} />
         <Route path="water" element={<WaterPage />} />
       </Routes>
